57734278fed827cb9af3ad20084c5706376f8a30
Artpyak <art.pyak@yandex.ru>
2025-06-27 18:38:04 +0300

    BJS2-66642 Изменил объявление логгера в KafkaCommentProducerImpl

--- src/main/java/faang/school/postservice/service/publisher/KafkaCommentProducerImpl.java
+++ src/main/java/faang/school/postservice/service/publisher/KafkaCommentProducerImpl.java
@@ -2,18 +2,17 @@ package faang.school.postservice.service.publisher;
 
 import faang.school.postservice.event.CommentCreatedEvent;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class KafkaCommentProducerImpl implements KafkaCommentProducer {
 
     private final KafkaTemplate<String, CommentCreatedEvent> kafkaTemplate;
-    private static final Logger log = LoggerFactory.getLogger(KafkaCommentProducerImpl.class);
 
     @Value("${kafka.topic.comments:comments}")
     private String topic;
